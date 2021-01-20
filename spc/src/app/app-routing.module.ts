ba0731fa21bd919721af7f72331ba064229886bf
Grant Lin <grant.lin@42-q.com>
2021-01-20 15:47:23 +0800

    feat(spc): update dependencies version

--- spc/src/app/app-routing.module.ts
+++ spc/src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { EmptyRouteComponent } from "./empty-route/empty-route.component";
-import { APP_BASE_HREF } from "@angular/common";
-
+import { APP_BASE_HREF } from '@angular/common';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 const routes: Routes = [
   {
