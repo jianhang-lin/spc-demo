0f7eae36cba0e81e15d648e7ea83d44611eb9dce
Grant Lin <grant.lin@42-q.com>
2021-01-20 11:24:13 +0800

    feat: init container code

---
+++ container/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SpaHostComponent } from "./spa-host/spa-host.component";
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SpaHostComponent
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
