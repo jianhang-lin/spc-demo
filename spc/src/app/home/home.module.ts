f6c29dd726a43fd5a207bc0c63557891fad80626
Grant Lin <grant.lin@42-q.com>
2021-01-21 12:18:05 +0800

    feat(home): add home card component

--- spc/src/app/home/home.module.ts
+++ spc/src/app/home/home.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeListComponent } from './home-list/home-list.component';
+import { HomeCardComponent } from './home-card/home-card.component';
+
 
 @NgModule({
   declarations: [
     HomeListComponent,
+    HomeCardComponent,
   ],
   exports: [
     HomeListComponent
   ],
   imports: [
     SharedModule,
-    HomeRoutingModule
+    HomeRoutingModule,
   ]
 })
 export class HomeModule { }
