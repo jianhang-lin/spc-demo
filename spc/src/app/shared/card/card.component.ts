24cb01d0af6f09a4b5fd999ef7db997724599f39
Grant Lin <grant.lin@42-q.com>
2021-01-21 11:49:47 +0800

    feat(shared): add card component

---
+++ spc/src/app/shared/card/card.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'spc-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  @Input() header = 'this is header';
+  @Input() footer = 'this is footer';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- spc/src/app/shared/shared.module.ts
+++ spc/src/app/shared/shared.module.ts
@@ -2,22 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
-
+import { SharedUiModule } from 'shared-ui';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CardComponent],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    SharedUiModule,
   ],
   exports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    SharedUiModule,
+    CardComponent,
   ],
   entryComponents: [
   ]
