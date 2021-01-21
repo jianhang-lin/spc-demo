4f941cfe061549b949d9546d028f9c05245f45e4
Grant Lin <grant.lin@42-q.com>
2021-01-21 16:01:56 +0800

    style(container): style code

--- app1/src/app/app-routing.module.ts
+++ app1/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { EmptyRouteComponent } from "./empty-route/empty-route.component";
-import {APP_BASE_HREF} from "@angular/common";
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 
 const routes: Routes = [
