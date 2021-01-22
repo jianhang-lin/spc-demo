45e1c279bfad1370134f2c0f5fb0f145f01cb03b
Grant Lin <grant.lin@42-q.com>
2021-01-22 14:30:11 +0800

    feat(app1): update app1

--- app1/src/app/app.module.ts
+++ app1/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmptyRouteComponent } from "./empty-route/empty-route.component";
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 @NgModule({
   declarations: [
