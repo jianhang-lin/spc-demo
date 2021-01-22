a1786556621f9d3d3d5788395fac19052eb9dde9
Grant Lin <grant.lin@42-q.com>
2021-01-22 15:03:18 +0800

    feat(navbar): update navbar project

--- navbar/src/app/app.module.ts
+++ navbar/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { PrimaryNavComponent } from './primary-nav/primary-nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmptyRouteComponent,
+    PrimaryNavComponent
   ],
   imports: [
     BrowserModule,
