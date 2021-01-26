ed67a012455e19d1b714ddcddb9bd396cc889abd
Grant Lin <grant.lin@42-q.com>
2021-01-26 10:33:27 +0800

    refactor(home): remove unused import

--- spc/src/app/home/home-routing.module.ts
+++ spc/src/app/home/home-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeListComponent } from './home-list/home-list.component';
 
 const routes: Routes = [
 ];
