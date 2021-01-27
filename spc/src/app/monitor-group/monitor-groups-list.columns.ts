7181d7c350fca09571e9c3bbdbe21e204581e5c4
Grant Lin <grant.lin@42-q.com>
2021-01-27 09:58:00 +0800

    feat(monitor-group): update list columns

--- spc/src/app/monitor-group/monitor-groups-list.columns.ts
+++ spc/src/app/monitor-group/monitor-groups-list.columns.ts
@@ -4,7 +4,7 @@ export enum UserDetailsType {
   FEDERATED = 'FEDERATED',
   NONFEDERATED = 'NONFEDERATED'
 }
-export let admin42QMonitorGroupsListHeaderColumns: Array<Column> = [
+export let monitorGroupsColumns42QAdmin: Array<Column> = [
            {
                header: 'monitor-groups-list.customer',
                field: 'customer',
@@ -120,91 +120,102 @@ export let admin42QMonitorGroupsListHeaderColumns: Array<Column> = [
            }
        ];
 
-export let siteAdminMonitorGroupsListHeaderColumns = [
-    {
-        header: 'monitor-groups-list.login-id',
-        field: 'loginId',
-        filterType: 'globalFilter',
-        filterMatchMode: 'LIKE',
-        placeholder: 'users-list.search-by-name-or-login',
-        isGlobal: true,
-        isDisplayed: true,
-        autofocus: true
-    },
-
-    { header: 'monitor-groups-list.name', field: 'userName', isDisplayed: true, autofocus: false },
-    {
-        header: 'users-list.email',
-        field: 'email',
-        filterType: 'inputFilter',
-        filterMatchMode: 'LIKE',
-        placeholder: 'users-list.search-by-email',
-        isDisplayed: true,
-        autofocus: false
-    },
-    {
-        header: 'monitor-groups-list.status',
-        field: 'active',
-        isBoolean: true,
-        isTrueLabel: 'Active',
-        isFalseLabel: 'Inactive',
-        filterType: 'searchableDropdownFilter',
-        placeholder: 'users-list.status',
-        filterMatchMode: 'EQUAL',
-        optionLabel: 'name',
-        uniqueKey: 'value',
-        filterPlaceholder: 'users-list.status-filter-placeholder',
-        isDisplayed: true,
-        autofocus: false
-    },
-    {
-        header: 'monitor-groups-list.user-type',
-        field: 'userType',
-        fieldIsKey: true,
-        objectKeysValues: [
-            { key: UserDetailsType.FEDERATED, label: 'users-list.federated', addClass: 'active-user' },
-            {
-                key: UserDetailsType.NONFEDERATED,
-                label: 'users-list.non-federated',
-                addClass: 'inactive-user'
-            }
-        ],
-        filterType: 'searchableDropdownFilter',
-        placeholder: 'users-list.user-type',
-        filterMatchMode: 'EQUAL',
-        optionLabel: 'name',
-        uniqueKey: 'value',
-        filterPlaceholder: 'users-list.user-type-filter-placeholder',
-        isDisplayed: true,
-        autofocus: false
-    },
-    {
-        header: 'monitor-groups-list.tags',
-        field: 'tags',
-        isArrayOfObjects: true,
-        fieldKey: 'name',
-        addClass: 'tag',
-        placeholder: 'general.label.search',
-        filterType: 'searchableDropdownFilter',
-        uniqueKey: 'id',
-        filterMatchMode: 'IN',
-        optionLabel: 'name',
-        filterPlaceholder: 'users-list.tags-filter-placeholder',
-        isDisplayed: true,
-        autofocus: false
-    },
-    {
-        header: 'monitor-groups-list.profile',
-        field: 'profiles',
-        isArrayOfObjects: true,
-        fieldKey: 'profileName',
-        placeholder: 'users-list.profiles',
-        filterType: 'searchableDropdownFilter',
-        uniqueKey: 'id',
-        filterMatchMode: 'IN',
-        optionLabel: 'profileName',
-        filterPlaceholder: 'users-list.profile-filter-placeholder',
-        isDisplayed: true,
-        autofocus: false
-    }
+export let monitorGroupsColumnsSite: Array<Column> = [
+  {
+    header: 'monitor-groups-list.position',
+    field: 'position',
+    filterType: 'globalFilter',
+    filterMatchMode: 'LIKE',
+    placeholder: 'monitor-groups-list.search-by-name-or-login',
+    isGlobal: true,
+    isDisplayed: true,
+    autofocus: true,
+    sortDisabled: false
+  },
+  {
+    header: 'monitor-groups-list.name',
+    field: 'userName',
+    isDisplayed: true,
+    autofocus: false,
+    sortDisabled: false
+  },
+  {
+    header: 'monitor-groups-list.email',
+    field: 'email',
+    filterType: 'inputFilter',
+    filterMatchMode: 'LIKE',
+    placeholder: 'monitor-groups-list.search-by-email',
+    isDisplayed: true,
+    autofocus: false,
+    sortDisabled: false
+  },
+  {
+    header: 'monitor-groups-list.status',
+    field: 'active',
+    isBoolean: true,
+    isTrueLabel: 'Active',
+    isFalseLabel: 'Inactive',
+    filterType: 'searchableDropdownFilter',
+    placeholder: 'monitor-groups-list.status',
+    filterMatchMode: 'EQUAL',
+    optionLabel: 'name',
+    uniqueKey: 'value',
+    filterPlaceholder: 'monitor-groups-list.status-filter-placeholder',
+    isDisplayed: true,
+    autofocus: false,
+    sortDisabled: false
+  },
+  {
+    header: 'monitor-groups-list.user-type',
+    field: 'userType',
+    fieldIsKey: true,
+    objectKeysValues: [
+        { key: UserDetailsType.FEDERATED, label: 'monitor-groups-list.federated', addClass: 'active-user' },
+        {
+            key: UserDetailsType.NONFEDERATED,
+            label: 'monitor-groups-list.non-federated',
+            addClass: 'inactive-user'
+        }
+    ],
+    filterType: 'searchableDropdownFilter',
+    placeholder: 'monitor-groups-list.user-type',
+    filterMatchMode: 'EQUAL',
+    optionLabel: 'name',
+    uniqueKey: 'value',
+    filterPlaceholder: 'monitor-groups-list.user-type-filter-placeholder',
+    isDisplayed: true,
+    autofocus: false,
+    sortDisabled: false
+  },
+  {
+    header: 'monitor-groups-list.tags',
+    field: 'tags',
+    isArrayOfObjects: true,
+    fieldKey: 'name',
+    addClass: 'tag',
+    placeholder: 'general.label.search',
+    filterType: 'searchableDropdownFilter',
+    uniqueKey: 'id',
+    filterMatchMode: 'IN',
+    optionLabel: 'name',
+    filterPlaceholder: 'monitor-groups-list.tags-filter-placeholder',
+    isDisplayed: true,
+    autofocus: false,
+    sortDisabled: false
+  },
+  {
+    header: 'monitor-groups-list.profile',
+    field: 'profiles',
+    isArrayOfObjects: true,
+    fieldKey: 'profileName',
+    placeholder: 'monitor-groups-list.profiles',
+    filterType: 'searchableDropdownFilter',
+    uniqueKey: 'id',
+    filterMatchMode: 'IN',
+    optionLabel: 'profileName',
+    filterPlaceholder: 'monitor-groups-list.profile-filter-placeholder',
+    isDisplayed: true,
+    autofocus: false,
+    sortDisabled: false
+  }
 ];
