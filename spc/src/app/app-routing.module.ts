a7432e1155104ac8bcc4ffb8d6d4e98e658f5f38
Grant Lin <grant.lin@42-q.com>
2021-01-27 11:40:37 +0800

    feat(app): implement app component

--- spc/src/app/app-routing.module.ts
+++ spc/src/app/app-routing.module.ts
@@ -2,12 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
-import { MonitorGroupListComponent } from './monitor-group/monitor-group-list/monitor-group-list.component';
-import { HomeListComponent } from './home/home-list/home-list.component';
 
 const routes: Routes = [
-  {path: 'spc', component: HomeListComponent},
-  {path: 'spc/monitor_groups', component: MonitorGroupListComponent},
   {
     path: '**',
     component: EmptyRouteComponent
--- spc/src/app/app.component.ts
+++ spc/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'spc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'spc';
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
+  title = 'This is SPC Project';
+  isHomePage: boolean;
+  ngOnInit(): void {
+    this.isHomePage = true;
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  isSpcPage() {
+    return window.location.pathname === '/spc';
+  }
 }
