7ebe15686914d6ecfd986a096838051bbab1e8f1
Grant Lin <grant.lin@42-q.com>
2021-01-27 12:27:20 +0800

    feat(app): add monitor module

---
+++ spc/src/app/monitor/monitor-home/monitor-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'spc-monitor-home',
+  templateUrl: './monitor-home.component.html',
+  styleUrls: ['./monitor-home.component.scss']
+})
+export class MonitorHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ spc/src/app/monitor/monitor.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MonitorHomeComponent } from './monitor-home/monitor-home.component';
+
+
+
+@NgModule({
+  declarations: [MonitorHomeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MonitorModule { }
