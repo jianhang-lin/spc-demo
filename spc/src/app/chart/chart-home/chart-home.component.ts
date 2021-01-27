20456ef071c441a1a0c581c7ac02c9baf2a196ff
Grant Lin <grant.lin@42-q.com>
2021-01-27 12:27:30 +0800

    feat(app): add chart module

---
+++ spc/src/app/chart/chart-home/chart-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'spc-chart-home',
+  templateUrl: './chart-home.component.html',
+  styleUrls: ['./chart-home.component.scss']
+})
+export class ChartHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ spc/src/app/chart/chart.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChartHomeComponent } from './chart-home/chart-home.component';
+
+
+
+@NgModule({
+  declarations: [ChartHomeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChartModule { }
