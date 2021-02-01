597e23c05a5b03d0ba5877026cd1a929519414e4
Grant Lin <grant.lin@42-q.com>
2021-02-01 13:51:45 +0800

    feat(system-parameter): add system-parameter list component

---
+++ spc/src/app/system-parameter/system-parameter-list/system-parameter-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'spc-system-parameter-list',
+  templateUrl: './system-parameter-list.component.html',
+  styleUrls: ['./system-parameter-list.component.scss']
+})
+export class SystemParameterListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
