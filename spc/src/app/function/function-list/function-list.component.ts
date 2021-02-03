0426e2aaf118b73c28fc9c995cbd19caa7217a2d
Grant Lin <grant.lin@42-q.com>
2021-02-03 13:56:40 +0800

    feat(function): implement function card click method

--- spc/src/app/function/function-list/function-list.component.ts
+++ spc/src/app/function/function-list/function-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HomeService } from '../../services/home.service';
 import { FunctionService } from '../../services/function.service';
 import { FunctionCard } from '../../domain/function-card.model';
+import { HomePageBuilder } from '../../domain/home-page.model';
 
 @Component({
   selector: 'spc-function-list',
@@ -12,6 +14,7 @@ export class FunctionListComponent implements OnInit {
 
   functionCards$: Observable<FunctionCard[]>;
   constructor(
+    private homeService: HomeService,
     private functionService: FunctionService
   ) { }
 
@@ -19,5 +22,33 @@ export class FunctionListComponent implements OnInit {
     this.functionCards$ = this.functionService.getFunctionCards();
     this.functionCards$.subscribe(functionCard => {
     });
+  }
+
+  onClickFunctionCard($event: MouseEvent, functionCard: FunctionCard) {
+    // call preventDefault to stop go to target url
+    $event.preventDefault();
+    switch (functionCard.url) {
+      case '/spc/monitors':
+        this.homeService.switchCurrentHomePage(new HomePageBuilder().getMonitorHomePage());
+        break;
+      case '/spc/emails':
+        this.homeService.switchCurrentHomePage(new HomePageBuilder().getMonitorHomePage());
+        break;
+      case '/spc/locks':
+        this.homeService.switchCurrentHomePage(new HomePageBuilder().getLockHomePage());
+        break;
+      case '/spc/location-families':
+        this.homeService.switchCurrentHomePage(new HomePageBuilder().getLocationFamilyHomePage());
+        break;
+      case '/spc/system-logs':
+        this.homeService.switchCurrentHomePage(new HomePageBuilder().getSystemLogHomePage());
+        break;
+      case '/spc/system-parameters':
+        this.homeService.switchCurrentHomePage(new HomePageBuilder().getSystemParameterHomePage());
+        break;
+      default:
+        this.homeService.switchCurrentHomePage(new HomePageBuilder().getDefaultHomePage());
+        break;
+    }
   }
 }
