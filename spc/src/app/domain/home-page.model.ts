bddff33324d83313b0f18958dfd303a12ed6c312
Grant Lin <grant.lin@42-q.com>
2021-02-03 11:43:55 +0800

    feat(app): update app layout html

--- spc/src/app/domain/home-page.model.ts
+++ spc/src/app/domain/home-page.model.ts
@@ -4,6 +4,11 @@ export interface HomePage {
   functionHomePage: boolean;
   monitorHomePage: boolean;
   chartHomePage: boolean;
+  emailHomePage: boolean;
+  lockHomePage: boolean;
+  locationFamilyHomePage: boolean;
+  systemLogHomePage: boolean;
+  systemParameterHomePage: boolean;
 }
 
 export class HomePageBuilder {
@@ -16,38 +21,112 @@ export class HomePageBuilder {
     monitorGroupHomePage: boolean,
     functionHomePage: boolean,
     monitorHomePage: boolean,
-    chartHomePage: boolean
+    chartHomePage: boolean,
+    emailHomePage: boolean,
+    lockHomePage: boolean,
+    locationFamilyHomePage: boolean,
+    systemLogHomePage: boolean,
+    systemParameterHomePage: boolean,
   ): HomePage {
     return {
       spcHomePage,
       monitorGroupHomePage,
       functionHomePage,
       monitorHomePage,
-      chartHomePage
+      chartHomePage,
+      emailHomePage,
+      lockHomePage,
+      locationFamilyHomePage,
+      systemLogHomePage,
+      systemParameterHomePage,
     };
   }
 
   public getDefaultHomePage(): HomePage {
-    return HomePageBuilder.homePage(true, false, false, false, false);
+    return HomePageBuilder.homePage(true, false,
+      false, false,
+      false, false,
+      false, false,
+      false, false);
   }
 
   public getSpcHomePage(): HomePage {
-    return HomePageBuilder.homePage(true, false, false, false, false);
+    return HomePageBuilder.homePage(true, false,
+      false, false,
+      false, false,
+      false, false,
+      false, false);
   }
 
   public getMonitorGroupHomePage(): HomePage {
-    return HomePageBuilder.homePage(false, true, false, false, false);
+    return HomePageBuilder.homePage(false, true,
+      false, false,
+      false, false,
+      false, false,
+      false, false);
   }
 
   public getFunctionHomePage(): HomePage {
-    return HomePageBuilder.homePage(false, false, true, false, false);
+    return HomePageBuilder.homePage(false, false,
+      true, false,
+      false, false,
+      false, false,
+      false, false);
   }
 
   public getMonitorHomePage(): HomePage {
-    return HomePageBuilder.homePage(false, false, false, true, false);
+    return HomePageBuilder.homePage(false, false,
+      false, true,
+      false, false,
+      false, false,
+      false, false);
   }
 
   public getChartHomePage(): HomePage {
-    return HomePageBuilder.homePage(false, false, false, false, true);
+    return HomePageBuilder.homePage(false, false,
+      false, false,
+      true, false,
+      false, false,
+      false, false);
+  }
+
+  public getEmailHomePage(): HomePage {
+    return HomePageBuilder.homePage(false, false,
+      false, false,
+      false, true,
+      false, false,
+      false, false);
+  }
+
+  public getLockHomePage(): HomePage {
+    return HomePageBuilder.homePage(false, false,
+      false, false,
+      false, false,
+      true, false,
+      false, false);
+  }
+
+  public getLocationFamilyHomePage(): HomePage {
+    return HomePageBuilder.homePage(false, false,
+      false, false,
+      false, false,
+      false, true,
+      false, false);
+  }
+
+  public getSystemLogHomePage(): HomePage {
+    return HomePageBuilder.homePage(false, false,
+      false, false,
+      false, false,
+      false, false,
+      true, false);
+  }
+
+  public getSystemParameterHomePage(): HomePage {
+    return HomePageBuilder.homePage(false, false,
+      false, false,
+      false, false,
+      false, false,
+      false, true);
   }
 }
